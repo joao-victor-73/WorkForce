bda86c9c5d13e51124eadad9be9e5e240725cb14
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-09-28 08:15:58 -0300

    criando interação para back+front
    Criando uma interação com o banco de dados para apresentar a lista de todos os funcionários já cadastrado no banco de dados.
    Criando também todos os Models para as tabelas.

--- projeto/main.py
+++ projeto/main.py
@@ -4,15 +4,68 @@ from flask_sqlalchemy import SQLAlchemy  # pip install flask-SQLAlchemy
 
 app = Flask(__name__)
 
+# Fazendo a conexão com o banco de dados através do SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:darc147@localhost/workforce'
 print('Banco de dados CONECTADO')
 
 db = SQLAlchemy(app)  # Instanciando a aplicação para o SQLAlchemy
 
 
+# Criando classe para as informações das tabelas
+
+class Departamentos(db.Model):
+    id_departamento = db.Column(
+        db.Integer, primary_key=True, autoincrement=True)
+    nome_departamento = db.Column(db.String(100))
+    id_gerente = db.Column(db.Integer, unique=True)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class Cargos(db.Model):
+    id_cargo = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_cargo = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class Funcionarios(db.Model):
+    id_func = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_func = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100))
+    tel1 = db.Column(db.String(15), nullable=False)
+    tel2 = db.Column(db.String(15))
+    data_contratacao = db.Column(db.Date, nullable=False)
+    salario = db.Column(db.Numeric(10, 2), nullable=False)
+    status_func = db.Column(
+        db.Enum('EFETIVO', 'FERIAS', 'DEMITIDO', 'ATESTADO'))
+    fk_id_cargo = db.Column(db.Integer)
+    fk_id_departamento = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class Folha_pagamento(db.Model):
+    id_pagamento = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    data_pagamento = db.Column(db.Date, nullable=False)
+    salario_base = db.Column(db.Numeric(10, 2), nullable=False)
+    deducoes = db.Column(db.Numeric(10, 2), nullable=False)
+    salario_liquido = db.Column(db.Numeric(10, 2), nullable=False)
+    fk_id_func = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# OBS: o db.Numeric é o equivalente ao Decimal do SQL. Esse é o tipo correspondente no SQLAlchemy.
+
+
 @app.route('/lista')
-def lista_funcionario():
-    return render_template('lista.html')
+def lista_de_funcionario():
+    lista_func = Funcionarios.query.order_by(Funcionarios.id_func)
+    return render_template('lista.html', lista_func=lista_func)
 
 
 @app.route('/cadastro')
