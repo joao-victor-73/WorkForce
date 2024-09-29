96971094e7f78ee136af80f3d48ad26104217079
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-09-28 22:18:43 -0300

    criando as FK corretamente

--- projeto/main.py
+++ projeto/main.py
@@ -14,6 +14,8 @@ db = SQLAlchemy(app)  # Instanciando a aplicação para o SQLAlchemy
 # Criando classe para as informações das tabelas
 
 class Departamentos(db.Model):
+    __tablename__ = 'departamentos'
+
     id_departamento = db.Column(
         db.Integer, primary_key=True, autoincrement=True)
     nome_departamento = db.Column(db.String(100))
@@ -24,6 +26,8 @@ class Departamentos(db.Model):
 
 
 class Cargos(db.Model):
+    __tablename__ = 'cargos'
+
     id_cargo = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome_cargo = db.Column(db.String(100))
 
@@ -32,6 +36,8 @@ class Cargos(db.Model):
 
 
 class Funcionarios(db.Model):
+    __tablename__ = 'funcionarios'
+
     id_func = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome_func = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100))
@@ -41,25 +47,46 @@ class Funcionarios(db.Model):
     salario = db.Column(db.Numeric(10, 2), nullable=False)
     status_func = db.Column(
         db.Enum('EFETIVO', 'FERIAS', 'DEMITIDO', 'ATESTADO'))
-    fk_id_cargo = db.Column(db.Integer)
-    fk_id_departamento = db.Column(db.Integer)
+
+    # Criando as foreign key da tabela
+    fk_id_cargo = db.Column(db.Integer, db.ForeignKey(
+        'cargos.id_cargo'), nullable=False)
+    fk_id_departamento = db.Column(db.Integer, db.ForeignKey(
+        'departamentos.id_departamento'), nullable=False)
+
+    # Criando as relações entre as tabelas
+    cargo = db.relationship('Cargos', backref='funcionarios')
+    departamento = db.relationship('Departamentos', backref='funcionarios')
 
     def __repr__(self):
         return '<Name %r>' % self.name
 
 
 class Folha_pagamento(db.Model):
+    __tablename__ = 'folha_pagamento'
+
     id_pagamento = db.Column(db.Integer, primary_key=True, autoincrement=True)
     data_pagamento = db.Column(db.Date, nullable=False)
     salario_base = db.Column(db.Numeric(10, 2), nullable=False)
     deducoes = db.Column(db.Numeric(10, 2), nullable=False)
     salario_liquido = db.Column(db.Numeric(10, 2), nullable=False)
-    fk_id_func = db.Column(db.Integer)
+
+    # Criando a Foreign Key da tabela
+    fk_id_func = db.Column(db.Integer, db.ForeignKey(
+        'funcionarios.id_func'), nullalbe=False)
+
+    # Criando a relação da tabela 'folha_pagamento' com 'funcionarios'
+    funcionario = db.relationship('Funcionarios', backref='folha_pagamentos')
 
     def __repr__(self):
         return '<Name %r>' % self.name
 
 # OBS: o db.Numeric é o equivalente ao Decimal do SQL. Esse é o tipo correspondente no SQLAlchemy.
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @app.route('/lista')
